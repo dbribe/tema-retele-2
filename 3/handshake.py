eb0816b9a5c0035818f95c3f7a1a6cb81ab47600
Denis <kir141996@gmail.com>
2018-05-26 20:56:05 +0300

    * 3 is “done”

--- 3/handshake.py
+++ 3/handshake.py
@@ -8,49 +8,52 @@ import time
 
 
 logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.NOTSET)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = 10394
-adresa = '198.13.0.14'
-server_address = (adresa, port)
 
 ip = IP()
 ip.src = '198.13.0.15'
 ip.dst = '198.13.0.14'
 tcp = TCP()
 tcp.sport = 6969
-tcp.dport = 10394
+tcp.dport = 11912
 tcp.seq = 100
 tcp.flags = 'S'
 
 SYN = ip/tcp
 raspuns_SYN_ACK = sr1(SYN)
-rasp_ack = raspuns_SYN_ACK.seq + 1
-rasp_seq = tcp.seq + 1
 
-tcp.seq = rasp_seq
-tcp.ack = rasp_ack
+tcp.seq += 1
+tcp.ack = raspuns_SYN_ACK.seq + 1
 tcp.flags = 'A'
 
-ACK= ip / tcp
+ACK = ip / tcp
 send (ACK)
 
 logging.info("Am terminat handshake-ul")
-sock.connect(server_address)
-logging.info("M-am conectat la server")
 
 options = [('MSS',2)]
-tcp.flags = 'PACE'
-
 ip.tos = int('011110' + '11', 2)
 
 
 for ch in "abc":
+    tcp.flags = 'PACE'
     tcp.ack = raspuns_SYN_ACK.seq + 1
-    rcv = sr1(ip/tcp/ch)
-    data = sock.recv(1)
-    logging.info("%s",data)
+    rcv = sr1(ip/tcp/ch, timeout=2, verbose=0)
+    rcv.show()
     tcp.seq += 1
+
+tcp.ack = raspuns_SYN_ACK.seq + 1
+rcv = sr1(ip/tcp/'abc')
+rcv.show()
+
+tcp.flags = 'R'
+rcv = sr1(ip/tcp)
+rcv.show()
+
+tcp.flags = 'PACE'
+tcp.ack = raspuns_SYN_ACK.seq + 1
+rcv = sr1(ip/tcp/'a')
+rcv.show()
 
 logging.info("Se incheie conexiunea")
 
-    
+
--- 3/server.py
+++ 3/server.py
@@ -1,26 +1,29 @@
 # TCP Server
+from scapy.all import *
 import socket
 import logging
 import time
 
-logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.NOTSET)
+logging.basicConfig(format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=logging.NOTSET)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-port = 10394
+
+port = 11912
 adresa = '198.13.0.14'
 server_address = (adresa, port)
 sock.bind(server_address)
 logging.info("Serverul a pornit pe %s si portul %d", adresa, port)
 sock.listen(1)
-logging.info('Asteptam conexiui...')
+logging.info('Asteptam conexiuni...')
 conexiune, address = sock.accept()
 logging.info("S-a conectat cineva")
-while True: 
+while True:
     data = conexiune.recv(1)
     if data == -1:
-      continue 
+        break
     logging.info('"%s"', data)
-    conexiune.send('A')
+
+logging.info("Ending connection")
+
 conexiune.close()
 sock.close()
