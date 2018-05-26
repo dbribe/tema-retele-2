584cd524752a29415e18db691c673a43162091ac
Stefan <robustefan12@gmail.com>
2018-05-26 15:37:31 +0300

    progres

--- 3/handshake.py
+++ 3/handshake.py
@@ -1,22 +1,39 @@
+# inainte de toate trebuie adaugata o regula de ignorare 
+# a pachetelor RST pe care ni le livreaza kernelul automat
+# iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
 from scapy.all import *
 
 ip = IP()
-ip.src = '198.13.0.14'
-ip.dst = '198.13.0.15'
-
-
+ip.src = '198.13.0.15'
+ip.dst = '198.13.0.14'
 tcp = TCP()
-tcp.sport = 54321
+tcp.sport = 6969
 tcp.dport = 10000
-
-## SYN ##
 tcp.seq = 100
-tcp.flags = 'S' # flag de SYN
-raspuns_syn_ack = sr1(ip/tcp)
+tcp.flags = 'S'
 
-tcp.seq += 1
-tcp.ack = raspuns_syn_ack.seq + 1
+SYN = ip/tcp
+raspuns_SYN_ACK = sr1(SYN)
+rasp_ack = raspuns_SYN_ACK.seq + 1
+rasp_seq = tcp.seq + 1
+
+tcp.seq = rasp_seq
+tcp.ack = rasp_ack
 tcp.flags = 'A'
-ACK = ip / tcp
 
-send(ACK)
+ACK= ip / tcp
+send (ACK)
+
+logging.info("Am terminat handshake-ul");
+
+options=[('MSS', 536)]
+
+tcp.flags = 'CE'
+
+int('DSCP_BINARY_STR' + 'ECN_BINARY_STR', 2)
+ip.tos = int('011100' + '11', 2)
+
+for ch in "abc":
+    rcv = sr1(ip/tcp/ch)
+    rcv
+    
--- 3/server.py
+++ 3/server.py
@@ -8,13 +8,14 @@ logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
 port = 10000
-adresa = 'localhost'
+adresa = '198.13.0.14'
 server_address = (adresa, port)
 sock.bind(server_address)
 logging.info("Serverul a pornit pe %s si portul %d", adresa, port)
 sock.listen(1)
 logging.info('Asteptam conexiui...')
 conexiune, address = sock.accept()
+logging.info("S-a conectat cineva")
 while True: 
     data = conexiune.recv(1)
     if data == -1:
